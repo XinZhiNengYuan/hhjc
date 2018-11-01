72aa7d5a0d26244a5bd0b59098418989bea8299f
shuidaimeng <253815059@qq.com>
2018-11-01 10:08:11 +0800

    扩展String类

--- huanghuajichang/DeviceManagement/common.swift
+++ huanghuajichang/DeviceManagement/common.swift
@@ -109,3 +109,20 @@ class common : NSObject{
 //        self.present(alertView,animated:true,completion:nil)
     }
 }
+
+//MARK:将unicode编码转为汉字
+extension String {
+    var unicodeStr:String {
+        let tempStr1 = self.replacingOccurrences(of: "\\u", with: "\\U")
+        let tempStr2 = tempStr1.replacingOccurrences(of: "\"", with: "\\\"")
+        let tempStr3 = "\"".appending(tempStr2).appending("\"")
+        let tempData = tempStr3.data(using: String.Encoding.utf8)
+        var returnStr:String = ""
+        do {
+            returnStr = try PropertyListSerialization.propertyList(from: tempData!, options: [.mutableContainers], format: nil) as! String
+        } catch {
+            print(error)
+        }
+        return returnStr.replacingOccurrences(of: "\\r\\n", with: "\n")
+    }
+}
